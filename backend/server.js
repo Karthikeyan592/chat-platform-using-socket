67475a6375bce352e79742cba8f755ade228df9d
KARTHIK MANIAN <20pw16@psgtech.ac.in>
2022-05-24 07:18:37 +0530

    adding dummy chat data for testing + ENV files

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const {chats} = require("./data/data");
 const dotenv = require("dotenv");
 
 const app = express();
@@ -10,8 +11,14 @@ app.get('/', function(req,res){
     res.send("API is running. . . .")
 })
 
-// app.get('/api/chat', function(req,res){
-//     res.send(chats)
-// })
+app.get('/api/chat', function(req,res){
+    res.send(chats)
+})
+
+app.get('/api/chat/:id', function(req,res){
+    // console.log(req.params.id);
+    const singlechat= chats.find(c=>c._id===req.params.id);
+    res.send(singlechat);
+})
 
 app.listen(PORT, console.log(`server started on ${PORT}`))
