284ba435f288ae58fbe85d8abe0788368c5812ee
KARTHIK MANIAN <20pw16@psgtech.ac.in>
2022-05-30 21:26:48 +0530

    port

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,13 @@ import './App.css';
 // import { Button } from '@chakra-ui/react';
 import { Route } from 'react-router-dom';
 import homepage from './Pages/homepage';
-import chatpage from './Pages/chatpage';
+import Chatpage from './Pages/chatpage';
 
 function App() {
   return (
     <div className="App">
       <Route path="/" component={homepage} exact/>
-      <Route path="/chats" component={chatpage} />
+      <Route path="/chats" component={Chatpage} />
       {}
     </div>
   );
