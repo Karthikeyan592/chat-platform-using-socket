9c72ad19096d59489b99c9360662e886e0922418
karthi ramu <20pw17@psgtech.ac.in>
2022-06-07 02:16:10 +0530

    part11-20min-done

--- frontend/src/Pages/chatpage.js
+++ frontend/src/Pages/chatpage.js
@@ -3,8 +3,9 @@ import { Box } from "@chakra-ui/layout";
 import {ChatState} from '../Context/ChatProvider';
 import MyChats from "../components/MyChats";
 import SideDrawer from "../components/miscellaneous/SideDrawer";
-import Chatbox from "../components/ChatBox";
 
+import ChatBox from '../components/ChatBox';
+ 
 const Chatpage = () => {
     const [fetchAgain,setFetchAgain] = useState(false);
     const { user } = ChatState();
@@ -12,11 +13,15 @@ const Chatpage = () => {
   return (
     <div style={{ width: "100%" }}>
       {user && <SideDrawer />}
-      <Box d="flex" justifyContent="space-between" w="100%" h="91.5vh" p="10px">
-        {user && <MyChats fetchAgain={fetchAgain} />}
-        {user && (
-          <Chatbox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
-        )}
+      <Box 
+        d="flex" 
+        justifyContent="space-between" 
+        w="100%" 
+        h="91.5vh" 
+        p="10px"
+      >
+        {user && <MyChats/>}
+        {user && <ChatBox/>}
       </Box>
     </div>
   )
--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -1,8 +1,60 @@
-import React from 'react'
+import { Box, Text } from "@chakra-ui/layout";
+import {
+  Avatar,
+  Menu,
+  MenuButton,
+  Tooltip
+} from "@chakra-ui/react";
+import { BellIcon, ChevronDownIcon } from "@chakra-ui/icons";
+import { Button } from "@chakra-ui/button";
+import { useState } from "react";
 
 const SideDrawer = () => {
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [loadingChat, setLoadingChat] = useState(false);
+  
   return (
-    <div>SideDrawer</div>
+    <div>
+      <Box
+        d="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        bg="white"
+        w="100%"
+        p="5px 10px 5px 10px"
+        borderWidth="5px"
+      >
+        <Tooltip label="Search Users to chat" hasArrow placement="bottom-end">
+          <Button variant="ghost">
+            <i className="fas fa-search"></i>
+            <Text d={{ base: "none", md: "flex" }} px={4}>
+              Search User
+            </Text>
+          </Button>
+        </Tooltip>
+        <Text fontSize="2xl" fontFamily="Work sans">
+          Talk-A-Tive
+        </Text>
+        <div>
+          <Menu>
+            <MenuButton p={1}>
+
+              <BellIcon/>
+            </MenuButton>
+            {/* <MenuList></MenuList> */}
+            
+          </Menu>
+          <Menu>
+            <MenuButton as={Button} righticon={<ChevronDownIcon />}>
+               <Avatar size="sm" cursor="pointer" name=''/>
+            </MenuButton>
+            
+          </Menu>
+        </div>
+      </Box>
+    </div>
   )
 }
 
