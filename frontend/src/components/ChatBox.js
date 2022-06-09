9e352c8686ca1b327fb31592a3ceee57da8b522e
KARTHIK MANIAN <20pw16@psgtech.ac.in>
2022-06-09 07:40:12 +0530

    user-select-complete

--- frontend/src/components/ChatBox.js
+++ frontend/src/components/ChatBox.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Box } from "@chakra-ui/layout";
 
 const ChatBox = () => {
   return (
-    <div>ChatBox</div>
+    <Box
+    alignItems="center"
+      flexDir="column"
+      p={3}
+      bg="white"
+      w={{ base: "100%", md: "68%" }}
+      borderRadius="lg"
+      borderWidth="1px"
+    >
+      . . .
+    </Box>
   )
 }
 
