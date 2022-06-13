b634e65482e74afde82ee8a2f4b1f7f911d3bfda
KARTHIK MANIAN <20pw16@psgtech.ac.in>
2022-06-14 03:26:25 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,9 @@ connectDB();
 //accepting json data
 app.use(express.json()); 
 
-app.get('/', function(req,res){
-    res.send("API is running. . . .")
-})
+// app.get('/', function(req,res){
+//     res.send("API is running. . . .")
+// })
 
 app.use('/api/user',userRoutes);
 app.use('/api/chat',chatRoutes);
@@ -26,6 +26,9 @@ app.use("/api/message",messageRoutes);
 
 // ----------------deployment---------------
 const __dirname1 = path.resolve();
+// app.use(express.static(path.join(__dirname1,"/frontend/build")));
+
+// app.get('*',(req,res)=> {res.sendFile(path.resolve(__dirname1,'frontend','build','index.html'))});
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname1,"/frontend/build")));
 
